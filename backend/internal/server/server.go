cb28468527c143ae36780da13ff26f7afdb9320a
arnavsx3 <arnavsx3@gmail.com>
2026-06-27 17:07:37 +0530

    add Shutdown method to struct Server

--- backend/internal/server/server.go
+++ backend/internal/server/server.go
@@ -39,3 +39,11 @@ func New(cfg config.Config) *Server {
 		httpServer: httpServer,
 	}
 }
+
+func (s *Server) Start() error {
+	return s.httpServer.ListenAndServe()
+}
+
+func (s *Server) Shutdown(ctx context.Context) error {
+	return s.httpServer.Shutdown(ctx)
+}
